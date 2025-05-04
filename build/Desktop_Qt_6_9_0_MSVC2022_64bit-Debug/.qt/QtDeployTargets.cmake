3ce3f7c8cb32542f34c5fa64632e898e6e447dc3
Rghjr <krystianstk2000@gmail.com>
2025-05-04 21:20:00 +0200

    Masz usuniete

--- build/Desktop_Qt_6_9_0_MSVC2022_64bit-Debug/.qt/QtDeployTargets.cmake
+++
@@ -1,3 +0,0 @@
-set(__QT_DEPLOY_TARGET_Slots_FILE C:/Users/kryst/Desktop/Studia/Języki i paradygmaty programowania/Sem II/Slots/build/Desktop_Qt_6_9_0_MSVC2022_64bit-Debug/Slots.exe)
-set(__QT_DEPLOY_TARGET_Slots_TYPE EXECUTABLE)
-set(__QT_DEPLOY_TARGET_Slots_RUNTIME_DLLS D:/Programy/QT/6.9.0/msvc2022_64/bin/Qt6Widgetsd.dll;D:/Programy/QT/6.9.0/msvc2022_64/bin/Qt6Guid.dll;D:/Programy/QT/6.9.0/msvc2022_64/bin/Qt6Cored.dll)
