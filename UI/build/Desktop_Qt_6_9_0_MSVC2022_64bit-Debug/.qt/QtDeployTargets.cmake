8a7fd022f8ca797d1fb44842fe834d4e383a7bca
Krystian Strzępek <krystianstk2000@gmail.com>
2025-05-17 20:55:07 +0200

    Dodalem Bonus

---
+++ UI/build/Desktop_Qt_6_9_0_MSVC2022_64bit-Debug/.qt/QtDeployTargets.cmake
@@ -0,0 +1,2 @@
+set(__QT_DEPLOY_TARGET_UI_FILE C:/Users/kryst/Desktop/Studia/Języki i paradygmaty programowania/Sem II/Slots/UI/build/Desktop_Qt_6_9_0_MSVC2022_64bit-Debug/UI.lib)
+set(__QT_DEPLOY_TARGET_UI_TYPE STATIC_LIBRARY)
