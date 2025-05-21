04c6455c11252ef074f229baae2e77c443bd41cf
Bartek <bartek021005@outlook.com>
2025-05-21 16:27:32 +0200

    Rozdzieliłem gre 1 na biznes i ui

--- UI/oknogra1.h
+++ UI/oknogra1.h
@@ -1,6 +1,7 @@
 #ifndef OKNOGRA1_H
 #define OKNOGRA1_H
 
+#include "gra1mechaniki.h"
 #include <QLabel>
 #include <QMainWindow>
 #include <QVector>
@@ -18,32 +19,28 @@ class OknoGra1 : public QMainWindow
 public:
     explicit OknoGra1(QWidget *parent = nullptr);
     ~OknoGra1();
-    void UstawGrid(int **tablica, int rows, int cols);
+    void UstawGrid(QVector<QVector<int>> tablica, int rows, int cols);
     void AnimujGrawitacje();
     void Autospin(bool checked);
     void UsunPolaczoneOwoce(QString a);
     void LosujOdNowa();
     void WczytajPrawdopodobienstwa();
-    int PrzypiszOwocek(); // Dodajemy deklarację funkcji PrzypiszOwocek()
     void AktualizujSaldo();
     void SprawdzWygrana();
     void Bonus();
     void ZwiekszStawke();
     void ZmniejszStawke();
 
-    // Zmienna do przechowywania prawdopodobieństw
-    int p_japko, p_banan, p_winogrono, p_wisnia, p_ananas, p_kiwi, p_bonus, sumaProporcji;
-    int l_japko, l_banan, l_winogrono, l_wisnia, l_ananas, l_kiwi, l_bonus;
-    int l_freespin, l_dodatkowychFreeSpinow;
-    float w_japko, w_banan, w_winogrono, w_wisnia, w_ananas, w_kiwi;
     QTimer* autoSpinTimer = nullptr;
 
 
 private:
     Ui::OknoGra1 *ui;
     QVector<QVector<QLabel *>> gridLabels; // przechowujemy QLabel'e
+    Gra1Mechaniki* mechanika;
     float saldo = 100;
     int stawka = 5;
+    int sumaProporcji;
 };
 
 #endif // OKNOGRA1_H
--- WarstwaBiznesowa/warstwabiznesowa.cpp
+++
@@ -1,3 +0,0 @@
-#include "warstwabiznesowa.h"
-
-WarstwaBiznesowa::WarstwaBiznesowa() {}
